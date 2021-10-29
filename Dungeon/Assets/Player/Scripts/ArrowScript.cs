479976239d487b0c794124448fa4c4ad887193ae
Hubert Radzewicz <Hubert.rdza@gmail.com>
2021-10-29 23:41:28 +0200

    Added test gorunds

---
+++ Dungeon/Assets/Player/Scripts/ArrowScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 10f);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
